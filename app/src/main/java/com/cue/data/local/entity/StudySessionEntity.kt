f277e66133114ebe0c61053691924151db2704e4
intNation <mandlanation3@gmail.com>
2026-03-23 18:35:57 +0200

    added a studySession room entity

---
+++ app/src/main/java/com/cue/data/local/entity/StudySessionEntity.kt
@@ -0,0 +1,26 @@
+package com.cue.data.local.entity
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "StudySession")
+data class StudySessionEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    @ColumnInfo(name = "start_time")
+    val startTime: Long,
+
+    @ColumnInfo(name = "end_time")
+    val endTime: Long?,
+
+    @ColumnInfo(name = "duration")
+    val duration: Long,
+
+    @ColumnInfo(name = "end_type")
+    val endType: String? , // Manual or SEMI auto
+
+    @ColumnInfo(name = "created_at")
+    val createdAt : Long = System.currentTimeMillis(),
+          
+)
